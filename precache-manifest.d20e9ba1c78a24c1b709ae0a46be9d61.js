7fa55acd2aec2b6c90f7a5545c3bf237a20ab93f
Yeseo <superior817@naver.com>
2020-07-14 22:16:29 +0900

    Updates

---
+++ precache-manifest.d20e9ba1c78a24c1b709ae0a46be9d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c900c6fbd28a4d2c23077fa44b67796",
+    "url": "/momentum/index.html"
+  },
+  {
+    "revision": "bd4329e77f378da6c2a8",
+    "url": "/momentum/static/js/2.76a6ba40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/momentum/static/js/2.76a6ba40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50d7944a354b260de981",
+    "url": "/momentum/static/js/main.a8d6b883.chunk.js"
+  },
+  {
+    "revision": "7e71a9ccb82879f43485",
+    "url": "/momentum/static/js/runtime-main.d0c67512.js"
+  }
+]);
